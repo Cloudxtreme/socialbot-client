d5a9b11d87adc0e8473487900c3b757c5aa69079
Daniel Illouz <daniel@crowdynews.com>
2015-04-26 18:11:48 +0200

    Updated editor config and dependencies.
    - Editor config now supports 4 space indentation.
    - Added bootstrap.
    - Fixed indentation.

--- config/environment.js
+++ config/environment.js
@@ -1,54 +1,57 @@
 /* jshint node: true */
 
 module.exports = function(environment) {
-  var ENV = {
-    modulePrefix: 'socialbot-client',
-    environment: environment,
-    baseURL: '/',
-    locationType: 'auto',
-    EmberENV: {
-      FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
-      }
-    },
+	var ENV = {
+		modulePrefix: 'socialbot-client',
+		environment: environment,
+		baseURL: '/',
+		locationType: 'auto',
+		EmberENV: {
+			FEATURES: {
+				// Here you can enable experimental features on an ember canary build
+				// e.g. 'with-controller': true
+			}
+		},
 
-    APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
-    },
+		APP: {
+			// Here you can pass flags/options to your application instance
+			// when it is created
+		},
 
-    contentSecurityPolicy: {
-      'script-src': "'self' 'unsafe-eval' http://*:35729",
-      'font-src': "'self' http://fonts.gstatic.com",
-      'connect-src': "'self' *",
-      'style-src': "'self' 'unsafe-inline' http://fonts.googleapis.com",
-    }
-  };
+		contentSecurityPolicy: {
+			'script-src': "'self' 'unsafe-eval' http://*:35729",
+			'font-src': "'self' http://fonts.gstatic.com",
+			'connect-src': "'self' http://localhost:8888",
+			'style-src': "'self' 'unsafe-inline' http://fonts.googleapis.com",
+		}
+	};
 
-  if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
-  }
+	if (environment === 'development') {
+		// ENV.APP.LOG_RESOLVER = true;
+		// ENV.APP.LOG_ACTIVE_GENERATION = true;
+		// ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+		// ENV.APP.LOG_VIEW_LOOKUPS = true;
 
-  if (environment === 'test') {
-    // Testem prefers this...
-    ENV.baseURL = '/';
-    ENV.locationType = 'none';
+		ENV.APP.LOG_TRANSITIONS = true;
+		ENV.APP.API_HOST = 'http://localhost:8888';
+		ENV.APP.API_VERSION = 'v1';
+	}
 
-    // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
+	if (environment === 'test') {
+		// Testem prefers this...
+		ENV.baseURL = '/';
+		ENV.locationType = 'none';
 
-    ENV.APP.rootElement = '#ember-testing';
-  }
+		// keep test console output quieter
+		ENV.APP.LOG_ACTIVE_GENERATION = false;
+		ENV.APP.LOG_VIEW_LOOKUPS = false;
 
-  if (environment === 'production') {
+		ENV.APP.rootElement = '#ember-testing';
+	}
 
-  }
+	if (environment === 'production') {
+		//
+	}
 
-  return ENV;
+	return ENV;
 };
